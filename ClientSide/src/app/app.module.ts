bc9baf324b5f2da281dca216696b2e06d3c8668e
Freddan962 <fredde.sander@gmail.com>
2018-04-18 12:14:27 +0200

    Initial more event notifications implementation

--- ClientSide/src/app/app.module.ts
+++ ClientSide/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { MoreeventnotificationsPage } from './../pages/moreeventnotifications/moreeventnotifications';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    MoreeventnotificationsPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +31,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    MoreeventnotificationsPage
   ],
   providers: [
     StatusBar,
