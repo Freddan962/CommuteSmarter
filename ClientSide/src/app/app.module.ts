b08cfd630faa4f11da5a36825b7b7e7f9e9094e9
Daniel Holm <information@d-ank.com>
2018-04-18 10:24:34 +0200

    fix map tab

--- ClientSide/src/app/app.module.ts
+++ ClientSide/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { MapPage } from '../pages/map/map';
 import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
