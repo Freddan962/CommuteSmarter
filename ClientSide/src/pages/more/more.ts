457403a914bf6fa96fd0d89c00b8ebbef875d048
jopplas <c.johan.ericsson@gmail.com>
2018-04-18 13:17:52 +0200

    Linked all More-pages

--- ClientSide/src/pages/more/more.ts
+++ ClientSide/src/pages/more/more.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController, NavParams} from 'ionic-angular';
+
+import { MoreeventnotificationsPage } from '../moreeventnotifications/moreeventnotifications';
+import { MorelanguagePage } from '../morelanguage/morelanguage';
 
 @Component({
   selector: 'page-more',
@@ -7,8 +10,33 @@ import { NavController } from 'ionic-angular';
 })
 export class MorePage {
 
+
   constructor(public navCtrl: NavController) {
 
+    const signedIn = false;
+
   }
+
+
+
+  if(signedIn){
+    
+  }
+
+  openNotificationModal() {
+    this.navCtrl.push(MoreeventnotificationsPage);
+  }
+
+  openLanguageModal() {
+    this.navCtrl.push(MorelanguagePage);
+    // myModal.present();
+  }
+
+
+//   checkLoginState() {
+//   FB.getLoginStatus(function (response) {
+//     statusChangeCallback(response);
+//   });
+// }
 
 }
