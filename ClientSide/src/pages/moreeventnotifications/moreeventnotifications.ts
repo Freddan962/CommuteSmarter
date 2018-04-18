7ba75b49d12d76a00ffa79612f5e9f5fa31c9e68
Freddan962 <fredde.sander@gmail.com>
2018-04-18 16:06:06 +0200

    Component controlled notification settings generation

--- ClientSide/src/pages/moreeventnotifications/moreeventnotifications.ts
+++ ClientSide/src/pages/moreeventnotifications/moreeventnotifications.ts
@@ -15,11 +15,20 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MoreeventnotificationsPage {
 
+  notificationSettings: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
+    this.notificationSettings = [
+      {name: 'Road Work', ngModel: 'roadWorks', color: 'red'},
+      {name: 'Critical Obstacle', ngModel: 'criticalObstacle', color: 'red'},
+      {name: 'Obstacle', ngModel: 'obstacle', color: 'orange'},
+      {name: 'Road Closed', ngModel: 'roadClosed', color: 'red'},
+      {name: 'Traffic Jam', ngModel: 'trafficJam', color: 'orange'}
+    ];
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MoreeventnotificationsPage');
   }
-
 }
