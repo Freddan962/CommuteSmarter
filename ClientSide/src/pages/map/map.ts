5835d3810466f1d9c01f50276972072aac6c5739
Smedin <smedin97@gmail.com>
2018-04-18 10:59:35 +0200

    Google Maps API
    Added initial code for Maps API

--- ClientSide/src/pages/map/map.ts
+++ ClientSide/src/pages/map/map.ts
@@ -21,5 +21,11 @@ export class MapPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
   }
-
+	function initMap() {
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+    }
 }
