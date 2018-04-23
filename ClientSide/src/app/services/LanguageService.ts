d28ac116416b76eb1f5036abb8a5e6461dee961b
Freddan962 <fredde.sander@gmail.com>
2018-04-23 11:08:54 +0200

    LanguageService commenting & code refactoring

--- ClientSide/src/app/services/LanguageService.ts
+++ ClientSide/src/app/services/LanguageService.ts
@@ -6,33 +6,49 @@ import { Injectable } from '@angular/core';
 export class LanguageService {
   languages: any;
   storage: Storage;
+  translate: TranslateService;
 
   selectedLanguageID: any;
   
-  constructor(public storageService: Storage, public translate: TranslateService) { 
+  constructor(public storageService: Storage, public translateService: TranslateService) { 
     this.storage = storageService;
+    this.translate = translateService;
 
+    //Definition of languages LanguageService uses
     this.languages = [
       {id: 'en', name: 'English'},
       {id: 'sv', name: 'Svenska'}
     ];
 
-    translate.addLangs(this.getLanguageIDs());    
-    translate.setDefaultLang(this.getLanguageIDs[0]);
-    
+    //Prepare default settings for translation pipe
+    this.translate.addLangs(this.getLanguageIDs());    
+    this.translate.setDefaultLang(this.getLanguageIDs[0]);
+  
+    //Promise based call to fetch locally stored language identifier
     this.storage.get("language").then((id) => {
-      if (translate.currentLang == id)
+      if (this.translate.currentLang == id)
         return;
-
-      this.selectedLanguageID = id;
-      translate.use(this.getSelectedLanguageID());
+      
+        this.setLanguage(id);
     });
   }
 
+  /**
+   * getLanguages()
+   * 
+   * @returns LanguageService's language array 
+   * @memberof LanguageService
+  */
   getLanguages() {
     return this.languages;
   }
 
+  /**
+   * getLanguageIDs()
+   * 
+   * @returns All IDs from this LanguageService's language array
+   * @memberof LanguageService
+  */
   getLanguageIDs() {
     let ids = [];
     this.languages.forEach(language => {
@@ -41,10 +57,26 @@ export class LanguageService {
     return ids;
   }
 
+  /**
+   * getSelectedLanguageID()
+   * 
+   * @returns The currently selected language identifier
+   * @memberof LanguageService
+  */
   getSelectedLanguageID() {
     return this.selectedLanguageID;
   }
 
+  /**
+   * setLanguage(id)
+   * 
+   * Sets the language based on the provided ID for the application 
+   * and translation pipe.
+   * 
+   * @param {any} id The language to activate for the application  
+   * @memberof LanguageService
+   * @throws Will throw "Invalid language identifier" if a non-defined language ID is provided
+  */
   setLanguage(id) {
     let found = false;
     this.languages.forEach(language => {
@@ -57,5 +89,6 @@ export class LanguageService {
 
     this.storage.set("language", id);
     this.selectedLanguageID = id;
+    this.translate.use(this.getSelectedLanguageID());
   }
 }
