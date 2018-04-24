452ad6cf029132073c3ec1810650eaceb0c58b99
Daniel Holm <information@d-ank.com>
2018-04-24 10:40:42 +0200

    basic twitter login

--- ClientSide/src/pages/more/more.ts
+++ ClientSide/src/pages/more/more.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, ModalController, NavParams} from 'ionic-angular';
 
+import { TwitterConnect } from '@ionic-native/twitter-connect';
 import { MoreeventnotificationsPage } from '../moreeventnotifications/moreeventnotifications';
 import { MorelanguagePage } from '../morelanguage/morelanguage';
 
@@ -10,23 +11,26 @@ import { MorelanguagePage } from '../morelanguage/morelanguage';
 })
 export class MorePage {
 
+  signedIn: boolean;
 
-  constructor(public navCtrl: NavController) {
-    const signedIn = false;
+  constructor(public navCtrl: NavController, private twitter: TwitterConnect) {
+    signedIn = false;
   }
 
+  loginWithTwitter() {
+    this.twitter.login().then(onTwitterLoginSuccess, onTwitterLoginError);
+  }
 
-//For future register/profile/login-UI
-  // if(signedIn){
-  // }
+  onTwitterLoginSuccess(response) {
+    console.log(response);
+    signedIn = true;
+  }
 
-//   checkLoginState() {
-//   FB.getLoginStatus(function (response) {
-//     statusChangeCallback(response);
-//   });
-// }
+  onTwitterLoginError(error) {
+    console.log(error);
+  }
 
-//Open Notification page
+  //Open Notification page
   openNotificationModal() {
     this.navCtrl.push(MoreeventnotificationsPage);
   }
@@ -36,7 +40,5 @@ export class MorePage {
     this.navCtrl.push(MorelanguagePage);
   }
 
-
-
-
+  //ionic cordova plugin add https://github.com/chroa/twitter-connect-plugin --variable FABRIC_KEY=29ffbd50c9d16f409638495a50eec740d751fb3a --variable TWITTER_KEY=agC1meoWus9BLERR6pLKhbIk2 --variable TWITTER_SECRET=UWLv3b9hqZmm7fZXKoGZR0PWcstKYIwP0efJOoDp30umYjN3xs
 }
