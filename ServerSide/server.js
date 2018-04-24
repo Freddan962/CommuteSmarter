78bfce22a20951cd91e9855c35b002b8d12e5a26
Daniel Holm <information@d-ank.com>
2018-04-24 17:32:09 +0200

    add sequelize

--- ServerSide/server.js
+++ ServerSide/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 require('./app/routes')(app, {});
