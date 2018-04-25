f7122f01a0bfb2040470f2fb6c677e98ddb8280f
Nicole <nicole_granath@yahoo.se>
2018-04-25 14:07:58 +0200

    add reported

--- ServerSide/app/routes/events.js
+++ ServerSide/app/routes/events.js
@@ -5,16 +5,17 @@ module.exports = function(app, db) {
             color: req.body.color,
             location: req.body.location,
             coordinates: req.body.coordinates,
-            titel:req.body.titel
+            titel:req.body.titel,
+            reported:req.body.reported
         };
         events.push(event);
         res.json(event);
     });
 
     const events = [
-        {id: 1, color:'red', location: 'Torsgatan', coordinates: {lat: 54566456, long:5677998},titel:'Obstacle',},
-        {id: 2, color:'orange', location: 'Kungsgatan',coordinates: {lat: 54566456, long:5677998},titel:'closed for Marathon'},
-        {id: 3, color:'blue', location: 'Odengatan',coordinates: {lat: 54566456, long:5677998},titel:'emergency response vehicle'},
+        {id: 1, color:'red', location: 'Torsgatan', coordinates: {lat: 54566456, long:5677998},titel:'Obstacle', reported:'14:00'},
+        {id: 2, color:'orange', location: 'Kungsgatan',coordinates: {lat: 54566456, long:5677998},titel:'closed for Marathon',reported:'08:00'},
+        {id: 3, color:'blue', location: 'Odengatan',coordinates: {lat: 54566456, long:5677998},titel:'emergency response vehicle',report:'09:00'},
     ];
 
     app.get('/api/events', (req, res) => {
