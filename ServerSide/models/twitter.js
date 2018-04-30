be178b720637586f98ce4c1b6909eb9f63f618bb
Daniel Holm <information@d-ank.com>
2018-04-30 10:44:30 +0200

    add last login field

--- ServerSide/models/twitter.js
+++ ServerSide/models/twitter.js
@@ -3,7 +3,8 @@
 module.exports = (sequelize, DataTypes) => {
   var Twitter = sequelize.define('Twitter', {
     userId: DataTypes.STRING,
-    userToken: DataTypes.STRING
+    userToken: DataTypes.STRING,
+    lastLogin: DataTypes.STRING
   }, {});
 
   Twitter.associate = function(models) {};
