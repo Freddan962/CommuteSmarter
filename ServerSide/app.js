b240807f59abefeca44f1646d7f574900175dad3
Freddan962 <fredde.sander@gmail.com>
2018-05-02 14:14:50 +0200

    Fixed error handling with rendering engine

--- ServerSide/app.js
+++ ServerSide/app.js
@@ -9,16 +9,13 @@ var events = require('./routes/events');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', events);
+app.use(events);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,8 +29,10 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(500).json({
+    message: err.message,
+    error: err
+  });
 });
 
 module.exports = app;
