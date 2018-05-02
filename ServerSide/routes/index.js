19ea15a0c2ac069d036e95b30c3aa5e29e807f9e
Daniel Holm <information@d-ank.com>
2018-05-02 19:25:00 +0200

    add comment

--- ServerSide/routes/index.js
+++ ServerSide/routes/index.js
@@ -1,3 +1,11 @@
+/** routes/index.js
+ *
+ * In this file we require the files for each of the
+ * routes and export them in module.exports. With other
+ * words, we use this as an Index-file.
+ * Each file gets access to the app and database
+ * trough the parameters: app and modules.
+ */
 const twitterAccounts = require('./twitter');
 const events = require('./events');
 
