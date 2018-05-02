7c82b00bb84f0dcfb41c299dc1fb1381ab8b4e39
Daniel Holm <information@d-ank.com>
2018-05-02 19:18:23 +0200

    fix the tests

--- ServerSide/test/events.test.js
+++ ServerSide/test/events.test.js
@@ -1,5 +1,16 @@
+const request = require('supertest');
+const app = require('../app')
 
-it('adds 1 + 2 to equal 3', () => {
+describe('Test GET Events', () => {
+    test('Should repspond with HTTP status code 200', (done) => {
+        request(app).get('/api/events').then((response) => {
+            expect(response.statusCode).toBe(200);
+            done();
+        });
+    });
+});
+
+/*it('adds 1 + 2 to equal 3', () => {
     const sum = require('../routes/events');
     expect(sum(1, 2)).toBe(3);
-});
+});*/
--- ServerSide/test/twitter.test.js
+++ ServerSide/test/twitter.test.js
@@ -1,1 +1,12 @@
-const twitter = require('../routes/twitter');
+const request = require('supertest');
+const app = require('../app')
+
+//Just for now reposn with 500, change later
+describe('Test GET Twitter', () => {
+    test('Should repspond with HTTP status code 500', (done) => {
+        request(app).get('/api/twitter').then((response) => {
+            expect(response.statusCode).toBe(500);
+            done();
+        });
+    });
+});
