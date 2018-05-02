524c8e40c6d390898ba56d0329f2186f0a323273
Daniel Holm <information@d-ank.com>
2018-05-02 19:19:04 +0200

    fix typo

--- ServerSide/test/twitter.test.js
+++ ServerSide/test/twitter.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../app')
 
-//Just for now reposn with 500, change later
+//Just for now reposond with 500, change later
 describe('Test GET Twitter', () => {
     test('Should repspond with HTTP status code 500', (done) => {
         request(app).get('/api/twitter').then((response) => {
