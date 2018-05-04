83b6d39062a02c45815df627f541c7876355bb1e
Nicole <nicole_granath@yahoo.se>
2018-05-04 15:05:11 +0200

    fel i validcoordinates

--- ServerSide/services/get-coordinates.js
+++ ServerSide/services/get-coordinates.js
@@ -3,8 +3,13 @@ const httpMethods = require('./http.js');
 const hostname = 'openparking.stockholm.se';
 const request = '/LTF-Tolken/v1/ptillaten/all?outputFormat=json&maxFeatures=4500&apiKey=fc03a59a-04a6-4f07-8447-dd4634684d76'
 
-let populateDatabase = function(data) {
-    console.log(data);
+module.exports = function(app, models) {
+    let populateDatabase = function(data) {
+        console.log(data);
+    }
+    
+    let result = httpMethods.httpGetRequest(hostname, request, 443, populateDatabase);
+    models.ValidCoordinates.create(result.features, { fields: ['geometry.coordinates'] }).then(() => {
+        console.log("run");
+    });
 }
-
-let result = httpMethods.httpGetRequest(hostname, request, 443, populateDatabase);
