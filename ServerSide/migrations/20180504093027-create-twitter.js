c229b974ea2603901c814518af71da97e06fde4b
Daniel Holm <information@d-ank.com>
2018-05-07 16:51:39 +0200

    twitter migration

---
+++ ServerSide/migrations/20180504093027-create-twitter.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Twitters', {
+      userId: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+      },
+      userToken: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      lastLogin: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Twitters');
+  }
+};
