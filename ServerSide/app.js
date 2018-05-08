1511a84264ba9d67bfdc90beb941be5abcd73b75
Daniel Holm <information@d-ank.com>
2018-05-08 20:16:17 +0200

    allow cors

--- ServerSide/app.js
+++ ServerSide/app.js
@@ -13,6 +13,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 const models = require('./models');
 const workers = require('./workers');
 const coordinatesService = require('./services/get-coordinates');
