355771e1822ca6fd0cb37b4a8fb4eb803b89b15e
Daniel Holm <information@d-ank.com>
2018-05-16 18:29:13 +0200

    send twitter user id

--- ClientSide/src/app/services/eventsreportService.ts
+++ ClientSide/src/app/services/eventsreportService.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
-// import { HttpService } from './httpService';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { LoginWithTwitterService } from './loginWithTwitterService';
 
 @Injectable()
-export class EventsReportService {  
+export class EventsReportService {
 
-  constructor(/*private httpService: HttpService,*/ private http: Http) { 
-    // this.httpService = httpService;
-  }
+  constructor(
+    private http: Http,
+    private twitter: LoginWithTwitterService
+  )
+  {}
 
   sendReportToServer(report){
     const reqData = {
@@ -19,22 +21,23 @@ export class EventsReportService {
       reported: new Date(),
       category: report.category,
       description: report.description,
-      image: report.image
+      imageString: report.image,
+      userId: this.twitter.getUserId()
     }
 
     let headers = new Headers();
     headers.append('Content-Type', 'application/json')
 
+    //let url = 'http://10.0.1.16:3000/api/events';
+    let url = 'https://pvt73trafficinfo.herokuapp.com/api/events';
 
-    let url = 'http://localhost:3000/api/events'; 
-    // let url = 'https://pvt73trafficinfo.herokuapp.com/api/events'; //
     this.http.post(url, JSON.stringify(reqData), {headers: headers})
     .map(res => res.json())
     .subscribe(data => {
         console.log('server response:'),
         console.log(data)
-    }
-    )
+    });
+
     console.log('Report sent to server:')
   }
 }
