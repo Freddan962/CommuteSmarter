8289c08494ce07316bf31c1b092b5e1e94ad877e
Daniel Holm <information@d-ank.com>
2018-05-22 14:15:32 +0200

    change delete interval

--- ServerSide/workers/random-event-deletion.js
+++ ServerSide/workers/random-event-deletion.js
@@ -4,7 +4,7 @@ const sensor = require('../services/sensor.js');
 
 module.exports = {
   name: 'Random Event Deletion',
-  interval: 5500,
+  interval: 40000,
   func: function() {
     sensor.deleteRandomEvent(models,(() => {
       console.log("Called delete random event!");
