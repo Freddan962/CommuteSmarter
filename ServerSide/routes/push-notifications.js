ffb13f23dd9f577f818f7b8feee31a2afaa13494
Daniel Holm <information@d-ank.com>
2018-05-22 21:17:10 +0200

    add table and route to save push settings in

--- ServerSide/routes/push-notifications.js
+++ ServerSide/routes/push-notifications.js
@@ -2,10 +2,10 @@ const pushMethods = require('../services/push-notifications.js');
 
 module.exports = function(app, models) {
   /**
-  * Route for adding a new push user to the database.
-  * Requires the user to post a body containing the
-  * push id.
-  */
+   * Route for adding a new push user to the database.
+   * Requires the user to post a body containing the
+   * push id.
+   */
   app.post('/api/push/user', (request, result) => {
     let userId = request.body.userId;
     models.PushUsers.findOne({ where:{ userId: userId }}).then(userExists => {
@@ -21,6 +21,43 @@ module.exports = function(app, models) {
         }).catch((err) => {
           result.status(400).json({ staus: 400, error: err });
         });
+      }
+    });
+  });
+
+  /**
+   * Route for setting if an event category should send notifications.
+   */
+  app.post('/api/push/user/category/', (request, result) => {
+    let userId = request.body.userId;
+    let category = request.body.category;
+    let status = request.body.status;
+
+    console.log(userId);
+    console.log(category);
+    console.log(status);
+
+    models.PushUsers.findOne({ where: { userId: userId } }).then(userExists => {
+      if(userExists) {
+        models.PushSettings.findOne({ where:{ userId: userId, category: category }}).then(categoryExists => {
+          if(categoryExists) {
+            PushSettings.updateAttributes({
+              status: status
+            }).then(() => {
+              result.status(200).json({status: 200, message: 'Successfully changed status!'});
+            })
+          } else {
+            models.PushSettings.create({ userId: userId, category: category, status: status }).then(categoryExists => {
+              if(categoryExists) {
+                result.status(200).json({status: 200, message: 'Successfully changed status!'});
+              } else {
+                result.status(400).json({status: 400, message: 'Did not change status!'});
+              }
+            });
+          }
+        });
+      } else {
+        result.status(403).json({status: 403, message: 'Not a valid user!'});
       }
     });
   });
