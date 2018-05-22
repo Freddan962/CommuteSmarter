b62435fef98988fe2add3a1a10be19545c7f7f39
Daniel Holm <information@d-ank.com>
2018-05-22 11:32:47 +0200

    route for adding a push user

--- ServerSide/routes/push-notifications.js
+++ ServerSide/routes/push-notifications.js
@@ -6,7 +6,20 @@ module.exports = function(app, models) {
   * Requires the user to post a body containing the
   * push id.
   */
-  app.post('/api/push', (request, result) => {
-    
+  app.post('/api/push/user', (request, result) => {
+    let userId = request.body.userId;
+    models.PushUsers.findOne({ where:{ userId: userId }}).then(userExists => {
+      if(userExists) {
+        res.status(200).json({status: 200, message: "The push user already exists"});
+      } else {
+        models.PushUsers.create({request.body.userId}).then( pushUser => {
+          if(pushUser) {
+            res.status(200).json({status: 200, message: "Created the Push User Successfully"});
+          } else {
+            res.status(503).json({staus: 503, error: "Error occured on the server"});
+          }
+        });
+      }
+    })
   });
 }
